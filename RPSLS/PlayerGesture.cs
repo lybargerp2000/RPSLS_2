5446ecc8d77d1bbc7ebc482dc54a74c883624149
Patrick Lybarger <lybargerp2000@yahoo.com>
2020-02-11 15:45:36 -0600

    Created list for gestures.

---
+++ RPSLS/PlayerGesture.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    public abstract class PlayerGesture
+    {
+
+
+        List<string> gestures;
+
+        public PlayerGesture()
+        {
+            gestures = new List<string>();
+        }
+        public void AddToListOfGestures()
+        {
+            gestures.Add("Scissors");
+            gestures.Add("Paper");
+            gestures.Add("Rock");
+            gestures.Add("Lizard");
+            gestures.Add("Spock");
+
+
+        }
+
+    }
+}
